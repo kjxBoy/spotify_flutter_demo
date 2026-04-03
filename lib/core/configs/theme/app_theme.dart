1d795dbce17f759854046d347e0df415200cc5e1
kangjiaxing <kang.jiaxing@hellogroup.com>
2026-04-03 16:43:59 +0800

    完成了注册页面UI

--- lib/core/configs/theme/app_theme.dart
+++ lib/core/configs/theme/app_theme.dart
@@ -2,41 +2,74 @@ import 'package:flutter/material.dart';
 import 'package:spotify/core/configs/theme/app_colors.dart';
 
 class AppTheme {
-
   static final lightTheme = ThemeData(
     primaryColor: AppColors.primary,
     scaffoldBackgroundColor: AppColors.lightBackground,
     brightness: Brightness.light,
     fontFamily: 'satoshi',
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: Colors.transparent,
+      contentPadding: const EdgeInsets.all(30),
+      hintStyle: const TextStyle(
+        color: Color(0xff383838),
+        fontWeight: FontWeight.w500,
+      ),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: const BorderSide(width: 0.4, color: Colors.black),
+      ),
+    ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
-          elevation: 0,
-          backgroundColor: AppColors.primary,
-          foregroundColor: Colors.white,        // 文字颜色 👈 就是这个
-          textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-          shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30)
-        )
-      )
-    )
+        elevation: 0,
+        backgroundColor: AppColors.primary,
+        foregroundColor: Colors.white,
+        // 文字颜色 👈 就是这个
+        textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      ),
+    ),
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        foregroundColor: AppColors.primary,
+        textStyle: const TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
+      ),
+    ),
   );
 
   static final darkTheme = ThemeData(
-      primaryColor: AppColors.primary,
-      scaffoldBackgroundColor: AppColors.darkBackground,
-      brightness: Brightness.dark,
-      fontFamily: 'satoshi',
-      elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-              elevation: 0,
-              backgroundColor: AppColors.primary,
-              foregroundColor: Colors.white,
-              textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30)
-              )
-          )
-      )
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.darkBackground,
+    brightness: Brightness.dark,
+    fontFamily: 'satoshi',
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: Colors.transparent,
+      contentPadding: const EdgeInsets.all(30),
+      hintStyle: const TextStyle(
+        color: Color(0xffA7A7A7),
+        fontWeight: FontWeight.w500,
+      ),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: const BorderSide(width: 0.4, color: Colors.white),
+      ),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        elevation: 0,
+        backgroundColor: AppColors.primary,
+        foregroundColor: Colors.white,
+        textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      ),
+    ),
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        foregroundColor: AppColors.primary,
+        textStyle: const TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
+      ),
+    ),
   );
-
 }
--- lib/presentation/auth/pages/signup_or_signin.dart
+++ lib/presentation/auth/pages/signup_or_signin.dart
@@ -3,7 +3,9 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spotify/common/helpers/is_dark_mode.dart';
 import 'package:spotify/core/configs/assets/app_images.dart';
 import 'package:spotify/core/configs/assets/app_vectors.dart';
+import 'package:spotify/presentation/auth/pages/signup.dart';
 
+import '../../../common/widgets/appbar/app_bar.dart';
 import '../../../common/widgets/button/basic_app_button.dart';
 import '../../../core/configs/theme/app_colors.dart';
 
@@ -15,50 +17,33 @@ class SignupOrLogin extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
+          BasicAppbar(),
           Align(
             alignment: Alignment.topRight,
-            child: SvgPicture.asset(
-              AppVectors.topPattern
-            ),
+            child: SvgPicture.asset(AppVectors.topPattern),
           ),
           Align(
             alignment: Alignment.bottomRight,
-            child: SvgPicture.asset(
-              AppVectors.bottomPattern
-            ),
+            child: SvgPicture.asset(AppVectors.bottomPattern),
           ),
           Align(
             alignment: Alignment.bottomLeft,
-            child: Image.asset(
-              AppImages.authBG
-            ),
+            child: Image.asset(AppImages.authBG),
           ),
-
           Align(
             alignment: Alignment.center,
             child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 40
-              ),
+              padding: const EdgeInsets.symmetric(horizontal: 40),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  SvgPicture.asset(
-                    AppVectors.logo
-                  ),
-                  SizedBox(
-                    height: 55,
-                  ),
+                  SvgPicture.asset(AppVectors.logo),
+                  SizedBox(height: 55),
                   Text(
                     'Enjoy Listening To Music',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20
-                    ),
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
                   ),
-                  SizedBox(
-                    height: 20,
-                  ),
+                  SizedBox(height: 20),
                   Text(
                     'Spotify is a proprietary Swedish audio streaming and media services provider',
                     style: TextStyle(
@@ -68,16 +53,19 @@ class SignupOrLogin extends StatelessWidget {
                     ),
                     textAlign: TextAlign.center,
                   ),
-                  SizedBox(
-                    height: 30,
-                  ),
+                  SizedBox(height: 30),
                   Row(
                     children: [
                       Expanded(
                         flex: 1,
                         child: BasicAppButton(
                           onPressed: () {
-
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (BuildContext context) => SignupPage(),
+                              ),
+                            );
                           },
                           title: 'Register',
                         ),
@@ -86,23 +74,25 @@ class SignupOrLogin extends StatelessWidget {
                       Expanded(
                         flex: 1,
                         child: TextButton(
-                            onPressed: () {
-                            },
-                            child: Text(
-                              'Sign in',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                                color: context.isDarkMode ? Colors.white : Colors.black
-                              ),
-                        )),
-                      )
+                          onPressed: () {},
+                          child: Text(
+                            'Sign in',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: context.isDarkMode
+                                  ? Colors.white
+                                  : Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
                     ],
-                  )
+                  ),
                 ],
               ),
             ),
-          )
+          ),
         ],
       ),
     );
