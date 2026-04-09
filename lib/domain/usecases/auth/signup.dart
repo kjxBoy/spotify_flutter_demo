ac643314412cd51ae56c31d54a50a00ff1979a8d
kangjiaxing <kang.jiaxing@hellogroup.com>
2026-04-09 19:26:24 +0800

    补充文档

--- lib/domain/usecases/auth/signup.dart
+++ lib/domain/usecases/auth/signup.dart
@@ -5,6 +5,15 @@ import 'package:spotify/domain/repository/auth/auth.dart';
 
 import '../../../service_locator.dart';
 
+/*
+*
+* 在干净架构、领域驱动设计（DDD）、Flutter / 后端业务开发里，UseCase（用例） 一般指：
+* 专门处理 “一个业务动作” 的逻辑层，是业务逻辑的核心载体。
+*
+* 它不负责 UI、不负责数据库、不负责网络，
+* 只做一件事：接收参数 → 调用仓库 / 服务 → 返回结果。
+* */
+
 class SignupUseCase implements UseCase<Either,CreateUserReq> {
 
   @override
--- lib/presentation/root/pages/root.dart
+++ lib/presentation/root/pages/root.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:spotify/common/widgets/appbar/app_bar.dart';
 
 class RootPage extends StatelessWidget {
   const RootPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: const BasicAppbar(
+        title: Text("data"),
+      ),
+      body: Container(
+        color: Colors.red,
+      ),
+    );
   }
 }
